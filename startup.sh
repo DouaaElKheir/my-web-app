8275f5326cf08c502a3c067c3f823416351e311b
Douaa El kheir <douaa@Douaas-MacBook-Air.local>
2025-07-08 21:35:09 +0200

    Initial commit - App Node.js + Terraform

---
+++ startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y curl git nodejs npm
+
+mkdir -p /var/www/myapp
+cd /var/www/myapp
+
+git clone https://github.com/<TON-GITHUB>/my-web-app.git .
+npm install
+npm start &
+
